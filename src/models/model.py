a27c43baa63e7ced88c4f0ab501a31d94931d086
Peter Mačinec <peter.macinec@gmail.com>
2019-11-13 19:24:40 +0100

    ADD: prototype neural network

--- src/models/model.py
+++ src/models/model.py
@@ -1,21 +1,22 @@
 import tensorflow.keras as keras
 from tensorflow.keras import layers
+from tensorflow.keras.initializers import Constant
 
 
 class FakeNewsDetectionNet(keras.Model):
 
-    def __init__(self, dim_input, dim_embeddings, dim_output):
-        super(InceptionNet, self).__init__()
+    def __init__(self, dim_input, dim_embeddings, dim_output, embeddings):
+        super(FakeNewsDetectionNet, self).__init__()
         self.model_layers = [
             layers.Embedding(
                 input_dim=dim_input,
                 output_dim=dim_embeddings,
-                embeddings_initializer=keras.initializers.Constant(embedding_matrix),
+                embeddings_initializer=Constant(embeddings),
                 trainable=False
             ),
             layers.LSTM(128),
             layers.Dense(
-                units=512,
+                units=128,
                 activation='relu'
             ),
             layers.Dense(
